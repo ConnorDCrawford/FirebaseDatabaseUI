b6fa27542fb7142bfe711b39263ab5b280d7484d
Connor Crawford <cdcraw@icloud.com>
2016-11-01 16:02:22 -0400

    Initial Commit

---
+++ FirebaseDatabaseUI/FirebaseDatabaseUI.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseDatabaseUI.h
+//  FirebaseDatabaseUI
+//
+//  Created by Connor Crawford on 11/1/16.
+//  Copyright © 2016 Connor Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseDatabaseUI.
+FOUNDATION_EXPORT double FirebaseDatabaseUIVersionNumber;
+
+//! Project version string for FirebaseDatabaseUI.
+FOUNDATION_EXPORT const unsigned char FirebaseDatabaseUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseDatabaseUI/PublicHeader.h>
+
+
